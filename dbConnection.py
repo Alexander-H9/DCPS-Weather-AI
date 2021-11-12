cd653ee9385c034c9b3a3fde7d5ecc5514c374b4
Alexander-H9 <alexander.haerle@gmx.net>
2021-11-12 12:17:22 +0100

    db connection test

---
+++ dbConnection.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import datetime
+from influxdb import InfluxDBClient
+
+from random import randint
+from time import sleep
+
+class InfluxDB():
+
+    def __init__(self):
+        pass
+
+
+    def sendToInfluxDB(self, name, data):
+
+
+        # influx configuration
+        ifuser = "grafana"
+        ifpass = "dcpsaamr"
+        ifdb   = "home"
+        ifhost = "127.0.0.1"
+        ifport = 8086
+        measurement_name = name
+
+        # take a timestamp for this measurement
+        time = datetime.datetime.utcnow()
+
+
+
+        # format the data as a single measurement for influx
+        body = [
+            {
+                "measurement": measurement_name,
+                "time": time,
+                "fields": {
+                    "degree": data
+                }
+            }
+        ]
+
+        # connect to influx
+        ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
+
+        # write the measurement
+        ifclient.write_points(body)
+
+# while True:
+#     db = InfluxDB()
+#     rand = randint(0,360)
+#     db.sendToInfluxDB("anemometerTest", rand)
+#     print("wrote: ", rand)
+#     sleep(60)
--- direction.py
+++ direction.py
@@ -2,14 +2,23 @@
 
 from time import sleep
 import RPi.GPIO as GPIO
+from dbConnection import *
 
 class Sensor:
     def __init__(self):
         self.zero_crossing = 0
         self.phase_1 = 0
         self.phase_2 = 0
+        # calibrate becomes True whem zero_crossing is detected
+        self.calibrate = False
 
 sen = Sensor()
+db = InfluxDB()
+
+# test connection:
+for i in range(10):
+    db.sendToInfluxDB("anemometerTest", i)
+print("data send")
 
 channel1 = 37       # gpio pin 26
 channel2 = 36       # gpio pin 16
@@ -20,12 +29,17 @@ def calculate_degree(counter):
     deg = counter/1.388
     return int(deg)
 
+def callback_ch3(channel3):
+    sen.calibrate = True
+    print("Sensor is calibrated, DB connection can start now")
+
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(channel1, GPIO.IN)
 GPIO.setup(channel2, GPIO.IN)
 GPIO.setup(channel3, GPIO.IN)
+GPIO.add_event_detect(channel3, GPIO.RISING, callback=callback_ch3)
 
 
 phase_0 = GPIO.input(channel1)
@@ -65,9 +79,17 @@ while True:
     phase_0 = sen.phase_1
     
 
-    if i > 10:
+    if i > 50:
+        # let degree be always positive
+        if deg < 0:
+            deg += 360
+
         deg = calculate_degree(count)
         print("deg: ", deg)
         i = 0
         deg = calculate_degree(count)
         print("deg: ", deg)
+
+        if sen.calibrate:
+            db.sendToInfluxDB("anemometer-test", deg)
+
